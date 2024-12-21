41b9b89a85f87b425c9976cee2f64a5974f00553
1010000001111 <161914108+1010000001111@users.noreply.github.com>
2024-12-21 15:06:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import progress
 import cv2
 from yolov5 import YOLOv5
-import keyboard
 
 mod_path = 'Demo/runs/train/exp9_ok/weights/best.pt'
 Yolo_v5 = YOLOv5(mod_path)
+ep_robot = progress.EpRobot(connect = 'ap', kp = 70, ki = 5, kd = 30)
+ep_camera = ep_robot.my_camera
 
 def detect(cv2img, show_results = False):
     __img__ = cv2.cvtColor(cv2img, cv2.COLOR_BGR2RGB)
@@ -26,8 +27,9 @@ def detect(cv2img, show_results = False):
     return __boxes__, __scores__, __categories__, __names__
 
 def fruit(result):
-    while not keyboard.is_pressed('esc'):
-        img = progress.get_camera().read_cv2_image(strategy = "newest", timeout = 0.5)
+    count = 0
+    while count < 10:
+        img = ep_camera.read_cv2_image(strategy = "newest", timeout = 0.5)
         boxes, scores, categories, names = detect(img, show_results = False)
         flag = False
         for index, item in enumerate(boxes):
@@ -36,30 +38,25 @@ def fruit(result):
             x1, y1, x2, y2 = list(map(int, pts))
             print(names[t_class], x1, y1, x2, y2)
             if names[t_class] == result:
-                progress.seek(pos = x1, speed = 30)
+                tmp_y, tmp_x = progress.seek(chassis = ep_robot.my_chassis, pos = x1, speed = 5, target = 580, kx = 4000, ky = 3000, max_speed = 0.2)
+                if abs(tmp_y) <= 0.05 and abs(tmp_x) <= 0.05:
+                    count += 1
+                else:
+                    count = 0
                 flag = True
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.putText(img, names[t_class], (x1, y1), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=2, color=(0, 0, 0),
-                        thickness=1)
+            cv2.putText(img, names[t_class], (x1, y1), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=2, color=(0, 0, 0), thickness=1)
         cv2.imshow("EPCamera", img)
         if not flag:
-            progress.seek(pos = 0, speed = 30)
+            progress.seek(chassis = ep_robot.my_chassis, pos = 0, speed = 5, target = 580, kx = 4000, ky = 3000, max_speed = 0.2)
         cv2.waitKey(1)
+    ep_robot.my_chassis.drive_speed(x = 0, y = 0, z = 0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    progress.setup(kp = 30, ki = 10, kd = 50)
-    fruit(progress.get_result())
-    # progress.move(target_color = 'blue', base_speed = 70, start_angle = 180)
-    # progress.recognize()
-    # img = progress.get_image()
-    # boxes, scores, categories, names = detect(img, show_results=False)
-    # progress.seek(boxes, scores, categories, names, speed = 30)
-    # for i in range(2):
-    #     progress.move(base_speed = 20, start_angle = 0)
-    #     progress.seek()
-    #     progress.grab()
-    #     progress.move(base_speed = 20, start_angle = 0)
-    #     progress.place()
-    # while not keyboard.is_pressed('esc'):
-    #     pass
-    progress.end()
+    #ep_robot.result = progress.recognize(camera = ep_robot.my_camera)
+    fruit(ep_robot.result)
+    print("damn mother fucker")
+    progress.grab(arm = ep_robot.my_arm, gripper = ep_robot.my_gripper)
+    del ep_robot
+    pass
--- progress.py
+++ progress.py
@@ -12,6 +12,26 @@ line = []
 markers = []
 x, y, w, h = 0, 0, 0, 0
 
+class EpRobot:
+    def __init__(self, connect = 'ap', kp = 70, ki = 5, kd = 30):
+        self.my_robot = robot.Robot()
+        self.my_robot.initialize(connect)
+        self.my_chassis = self.my_robot.chassis
+        self.my_arm = self.my_robot.robotic_arm
+        self.my_arm.recenter().wait_for_completed()
+        self.my_gripper = self.my_robot.gripper
+        self.my_gripper.open()
+        self.my_vision = self.my_robot.vision
+        self.my_camera = self.my_robot.camera
+        self.my_camera.start_video_stream(display = False)
+        self.pid_ctrl = pid.PID(kp, ki, kd)
+        self.result = 'apple'
+        tof.setup()
+    def __del__(self):
+        self.my_camera.stop_video_stream()
+        self.my_robot.close()
+        tof.end()
+
 def on_detect_line(line_info):
     line.clear()
     print('line_type', line_info[0])
@@ -28,85 +48,74 @@ def on_detect_marker(marker_info):
         x, y, w, h, info = item
         markers.append(markerinfo.MarkerInfo(x, y, w, h, info))
 
-def setup(kp = 70, ki = 5, kd = 30):
-    global ep_robot, pid_ctrl
-    global ep_chassis, ep_arm, ep_camera, ep_vision, ep_gripper, ep_sensor
-    pid_ctrl = pid.PID(70, 5, 30)
-    ep_robot = robot.Robot()
-    ep_robot.initialize('ap')
-    ep_chassis = ep_robot.chassis
-    ep_arm = ep_robot.robotic_arm
-    ep_arm.recenter().wait_for_completed()
-    ep_gripper = ep_robot.gripper
-    ep_gripper.open()
-    ep_vision = ep_robot.vision
-    ep_camera = ep_robot.camera
-    ep_camera.start_video_stream(display = False)
-    tof.setup()
-
-def recognize():
-    global result
+def recognize(camera):
     result = ''
     while not result:
-        img = ep_camera.read_cv2_image(strategy = 'newest', timeout = 0.5)
+        img = camera.read_cv2_image(strategy='newest', timeout=0.5)
         pil_image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        text = pytesseract.image_to_string(pil_image, lang = 'eng').lower().split('\n\n')
+        text = pytesseract.image_to_string(pil_image, lang='eng').lower().split('\n\n')
         for string in ('apple', 'pineapple', 'watermelon', 'banana'):
             if string in text:
                 result = string
                 break
     print('recognize succeed: {}'.format(result))
+    return result
 
-def get_camera():
-    return ep_camera
-
-def get_result():
-    return 'apple'
-
-def trans_speed(x, target):
-    if x > target:
-        return x / target
-    else:
-        return -x / target + 2
-
-def seek(pos, speed = 30):
-    # if pos > 550:
-    #     ep_chassis.drive_wheels(w1=-speed, w2=speed, w3=-speed, w4=speed)
-    # else:
-    #     ep_chassis.drive_wheels(w1=speed, w2=-speed, w3=speed, w4=-speed)
-    s = speed * trans_speed(pos, 550)
-    if pos > 550:
-        s1, s3 = -s, -s
-        s2, s4 = s, s
-    else:
-        s1, s3 = s, s
-        s2, s4 = -s, -s
+def seek(chassis, pos, speed = 5, target = 550, kx = 1, ky = 1, max_speed = 0.2):
+    x_speed = 0
+    y_speed = 0
     distance = tof.dis()
-    if distance < 20:
-        err = trans_speed(distance, 30)
-        s1 -= err
-        s2 -= err
-        s3 -= err
-        s4 -= err
-    elif distance > 40:
-        err = trans_speed(distance, 30)
-        s1 += err
-        s2 += err
-        s3 += err
-        s4 += err
-    ep_chassis.drive_wheels(w1=s1, w2=s2, w3=s3, w4=s4)
-    pass
+    if abs(pos - target) > 10:
+        y_speed = speed * (pos - target) / kx
+    if abs(distance - 20) > 5:
+        x_speed = speed * (distance - 30) / ky
+    x_speed = x_speed if x_speed <= max_speed else max_speed
+    x_speed = x_speed if x_speed >= -max_speed else -max_speed
+    y_speed = y_speed if y_speed <= max_speed else max_speed
+    y_speed = y_speed if y_speed >= -max_speed else -max_speed
+    print(x_speed, y_speed)
+    chassis.drive_speed(x=x_speed, y=y_speed, z=0)
+    return y_speed, x_speed
 
-def grab():
-    pass
+def grab(arm, gripper):
+    print("catch start")
+    gripper.open(power=50)
+    time.sleep(3)
+    arm.recenter().wait_for_completed()
 
-def move(target_color = 'blue', base_speed = 20, start_angle = 0):
+    #time.sleep(1)
+    gripper.pause()
+    arm.move(x=0, y=90).wait_for_completed()  # TODO 调参
+    arm.move(x=140, y=0).wait_for_completed()  # TODO 调参
+    gripper.close(power=50)
+
+    #ime.sleep(1)
+    gripper.pause()
+    arm.move(x=-140, y=0).wait_for_completed()  # TODO 调参
+    arm.recenter().wait_for_completed()
+    print("catch end")
+
+def place(arm, gripper):
+    print("put start")
+    arm.move(x=0, y=90).wait_for_completed()  # TODO 调参
+    arm.move(x=140, y=0).wait_for_completed()  # TODO 调参
+    gripper.open(power=50)
+
+    #time.sleep(1)
+    arm.move(x=-120, y=0).wait_for_completed()  # TODO 调参
+    arm.recenter().wait_for_completed()
+
+    #time.sleep(1)
+    gripper.pause()
+    print("put end")
+
+def move(chassis, camera, vision, pid_ctrl, target_color = 'blue', base_speed = 20, start_angle = 0):
     if start_angle != 0: #turn left
-        ep_chassis.move(x = 0, y = 0, z = start_angle, z_speed = 180).wait_for_completed()
+        chassis.move(x = 0, y = 0, z = start_angle, z_speed = 180).wait_for_completed()
     quit_count = 0
-    result_line = ep_vision.sub_detect_info(name = 'line', color = target_color, callback = on_detect_line)
+    result_line = vision.sub_detect_info(name = 'line', color = target_color, callback = on_detect_line)
     while quit_count <= 10:
-        img = ep_camera.read_cv2_image(strategy = 'newest', timeout = 0.5)
+        img = camera.read_cv2_image(strategy = 'newest', timeout = 0.5)
         line_1 = line.copy()
         if line_1:
             quit_count = 0
@@ -120,7 +129,6 @@ def move(target_color = 'blue', base_speed = 20, start_angle = 0):
             if item.distance < min_distance:
                 min_distance = item.distance
                 min_err_x = item._x - 0.5
-
         #cv2.imshow('Line', img)
         cv2.waitKey(1)
         l_speed = 0
@@ -130,17 +138,9 @@ def move(target_color = 'blue', base_speed = 20, start_angle = 0):
             dif_speed = pid_ctrl.output
             l_speed = base_speed + dif_speed
             r_speed = base_speed - dif_speed
-        ep_chassis.drive_wheels(w2 = l_speed, w3 = l_speed, w1 = r_speed, w4 = r_speed)
+        chassis.drive_wheels(w2 = l_speed, w3 = l_speed, w1 = r_speed, w4 = r_speed)
         time.sleep(0.1)
-    ep_chassis.drive_wheels(0, 0, 0, 0)
-    result_line = ep_vision.unsub_detect_info(name = 'line')
+    chassis.drive_wheels(0, 0, 0, 0)
+    result_line = vision.unsub_detect_info(name = 'line')
     cv2.destroyAllWindows()
     print("move finished")
-
-def place():
-    pass
-
-def end():
-    ep_camera.stop_video_stream()
-    tof.end()
-    ep_robot.close()
